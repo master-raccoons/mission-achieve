d204ab8fd024bdb4b8d84fb9495f8877835cb993
neetesh patel <neetesh.patel@oracle.com>
2020-05-18 09:08:24 +0530

    Enhancement in complexity

--- Practice/Permutation.java
+++ Practice/Permutation.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 /**
  * This program is used to generate all possible permutations of a given string by using iterative method.
- * Complexity : Order of n^2*n!
+ * 1st Approach Complexity : Order of n^2*n!
  * Auxiliary Space: n!
  * Example : "abc"
  * Permutations Generated: abc,acb,bac,bca,cab,cba
- *  
+ *
+ * 2nd Approach Complexity : Order of n*n!
  */
 
 
@@ -16,7 +17,11 @@ public class Permutation
 public static void main(String ar[])
 {
     String str="abc";
-    generatePermute(str).stream().forEach(p->System.out.print(p+","));
+    //generatePermute(str).stream().forEach(p->System.out.print(p+","));
+    System.out.println();
+    Set set=new HashSet();
+    generatePermutationRecursively("",str,set).stream().forEach(s->System.out.println(s));
+
 
 }
 
@@ -45,6 +50,21 @@ public static void main(String ar[])
         return permutations;
     }
 
+
+    /*This method will work in those cases also, where characters are duplicate*/
+    private static Set generatePermutationRecursively(String result,String choices,Set set)
+    {
+
+        for(int i=0;i<choices.length();i++)
+        {
+            generatePermutationRecursively(result+choices.charAt(i)+"",choices.substring(0,i)+choices.substring(i+1),set);
+        }
+        if(choices.length()==0){
+            set.add(result);
+            return set;
+        }
+       return set;
+    }
 
 
 
